a56b30348d338e888482d3e7c9ad0ec928c5fb94
fengyang <--->
2023-05-15 11:18:41 +0800

    添加powershell相关配置

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,21 @@
+# 别名
+function vf { nvim $(fzf) }
+function o { explorer .}
+function lg { lazygit.exe }
+
+# powershell 主题配置
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_lean.omp.json" | Invoke-Expression
+
+# Set-PSReadLineOption -Colors @{ InlinePrediction = "`e[38;5;238m" }
+# ctrl+j 接收全部建议
+Set-PSReadLineKeyHandler -Chord "Ctrl+j" -Function ForwardChar
+# alt+j 接收下一个建议
+Set-PSReadLineKeyHandler -Chord "Alt+j" -Function ForwardWord
+
+# Get-ChildItem . -Recurse -Attributes Directory | Invoke-Fzf | Set-Location
+# $FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border  --preview 'cat {}'"
+Set-Item -Path 'Env:\FZF_DEFAULT_OPTS' -Value "--height 95% --layout=reverse  --preview 'bat --color=always --style=numbers --line-range=:500 {}' --prompt='❯ ' --info=inline:' ' --preview-window=down:70%"
+# Set-Item -Path 'Env:\FZF_DEFAULT_COMMAND' -Value "es"
+
+# 安装 z 插件
+# Install-Module -Name z 
--- README.md
+++ README.md
@@ -10,5 +10,8 @@
 * 使用管理员打开cmd
 * 执行`mklink /D  ~\AppData\Local\lf ~\dotfiles\lf`
 
+## powershell
 
-
+* 在powershell内输出powershell配置文件`echo $PROFILE`
+* 使用管理员打开cmd
+* `mklink powershell的配置文件路径 ~\dotfiles\Microsoft.PowerShell_profile.ps1`
