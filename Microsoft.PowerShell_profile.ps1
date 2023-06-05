8d832ed9dcb8391cb7b47c675a418728554fa9ec
fengyang <--->
2023-06-05 14:49:23 +0800

    添加软件安装、配置文档

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,13 @@
 # 别名
+function ll { lsd -l }
+function la { lsd -a }
+function lla { lsd -al }
+
 function vf { nvim $(fzf) }
 function nf { npp $(fzf) }
 function o { explorer .}
 function lg { lazygit.exe }
+function cdf { cd $(fd -td | fzf --height 55% --layout=reverse --prompt='❯ ' --info=inline:' ') }
 
 # powershell 主题配置
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/powerlevel10k_lean.omp.json" | Invoke-Expression
@@ -15,11 +20,11 @@ Set-PSReadLineKeyHandler -Chord "Alt+j" -Function ForwardWord
 
 # Get-ChildItem . -Recurse -Attributes Directory | Invoke-Fzf | Set-Location
 # $FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border  --preview 'cat {}'"
-Set-Item -Path 'Env:\FZF_DEFAULT_OPTS' -Value "--height 95% --layout=reverse --preview 'bat --color=always --style=numbers --line-range=:500 {}' --prompt='❯ ' --info=inline:' ' --preview-window=down:60%"
+# Set-Item -Path 'Env:\FZF_DEFAULT_OPTS' -Value "--height 95% --layout=reverse --preview 'bat --color=always --style=numbers --line-range=:500 {}' --prompt='❯ ' --info=inline:' ' --preview-window=down:60%"
 # Set-Item -Path 'Env:\FZF_DEFAULT_COMMAND' -Value "es"
 
 # 安装 z 插件
-# Install-Module -Name z 
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 # lf配置
 Set-Item -Path 'Env:\SHELL' -Value "pwsh"
 Set-Item -Path 'Env:\EDITOR' -Value "nvim"
--- README.md
+++ README.md
@@ -15,3 +15,34 @@
 * 在powershell内输出powershell配置文件`echo $PROFILE`
 * 使用管理员打开cmd
 * `mklink powershell的配置文件路径 ~\dotfiles\Microsoft.PowerShell_profile.ps1`
+
+## lsd
+
+* 安装
+    * `scoop install lsd`
+* 配置
+    * 使用管理员打开cmd
+    * `mklink C:\Users\YF\AppData\Roaming\lsd C:\Users\YF\dotfiles\lsd`
+
+## fd 
+
+* 安装
+    * `scoop install fd`
+
+## ripgrep 
+
+* 安装
+    * `scoop install ripgrep`
+
+## bat 
+
+* 安装
+    * `scoop install bat`
+
+## zoxide 
+
+* 安装
+    * `scoop install zoxide`
+* 配置
+    * 打开powershell配置文件：`nvim $PROFILE`
+    * `Invoke-Expression (& { (zoxide init powershell | Out-String) })`
